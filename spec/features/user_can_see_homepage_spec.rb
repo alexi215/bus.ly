068af331742a5d57762e336be728e347e59c5e27
alexi215 <alexi83@gmail.com>
2014-05-24 16:20:36 -0400

    installed more gems and configured Gemfile

--- spec/features/user_can_see_homepage_spec.rb
+++ spec/features/user_can_see_homepage_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe 'User can see homepage'
-  it 'has the title Bus.ly'
+describe 'User can see homepage'do
+  it 'has the title Bus.ly' do
     visit root_path
-    expect(page.to have_content) 'Bus.ly'
+    expect(page).to have_content 'Bus.ly'
   end
 end
