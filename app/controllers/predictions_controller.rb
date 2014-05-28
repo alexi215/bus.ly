2bc6986b931285e92145b77ce636bfbd936233e9
alexi215 <alexi83@gmail.com>
2014-05-28 13:48:14 -0400

    completed stops view

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,6 +1,5 @@
 class PredictionsController < ApplicationController
-  
-
+  # before_action :bus_route_id, only [:routes, :stops]
 
   def routes
     response = HTTParty.get('http://api.wmata.com/Bus.svc/json/JRoutes?api_key=ud7ahaty74qdc2ghs4h34y5g')
@@ -11,28 +10,30 @@ class PredictionsController < ApplicationController
     if params.has_key?(:route) && !params[:route].empty?
       response = HTTParty.get("http://api.wmata.com/Bus.svc/json/JRouteDetails?routeId=#{params[:route]}&api_key=ud7ahaty74qdc2ghs4h34y5g")
       @route_id           = response['RouteID']
+      
       @direction_0        = response['Direction0']
       @direction_0_sign   = response['Direction0']['TripHeadsign']
-      @direction_0_stops  = response['Direction0']['Stops'] 
+      @direction_0_stops  = response['Direction0']['Stops']
+      @stop_times         = response['Direction0']['StopTimes']
 
       @direction_1        = response['Direction1']
       @direction_1_sign   = response['Direction1']['TripHeadsign']
       @direction_1_stops  = response['Direction1']['Stops']
+      @stop_times         = response['Direction1']['StopTimes']
     else
       redirect_to bus_routes_path
     end
-  end
+  end # END of stops
 
   def times
-    if params.has_key?(:stop) && !params[:stop].empty?
-      response = HTTParty.get("http://api.wmata.com/NextBusService.svc/json/JPredictions?StopID=#{params[:stop]}&api_key=ud7ahaty74qdc2ghs4h34y5g")
-      @stop_name           = response['StopName']
-      @route               = response['Predictions']
+    if params[:stop] && params[:route]
+      response = HTTParty.get("http://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=#{params[:stop]}&api_key=ud7ahaty74qdc2ghs4h34y5g")
+      @predictions = response['Predictions'].select do |time|
+        time['RouteID'] == params[:route]
+      @stop_name = response['StopName']
+      end
     else
       redirect_to bus_stops_path
     end
-  end
-
-
-
-end # END OF PredictionsController
+  end # END of times
+end # END of PredictionsController
